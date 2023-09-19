b54a7ba5c2ed73b2905611340fd6d3022605d83d
Samuel-Prado <117532897+Samuel-Prado@users.noreply.github.com>
2023-09-19 12:26:48 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-	    Pessoa pessoa = New Pessoa()
+	    Pessoa pessoa = New Pessoa(1,"Samuel Prado Soares", "samuel@gmail.com");
+	    Pessoa pessoa = New Pessoa(2,"Victor Afonso Fernandes", "afonso@gmail.com");
+	    System.out.println(pessoa);
     }
 }
